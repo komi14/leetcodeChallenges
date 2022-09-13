b0b6353ff348c4dfa919b72f00fade54d6fada52
Komal Vishwakarma <72248965+komi14@users.noreply.github.com>
2022-09-13 19:08:41 +0530

    Time: 1 ms (100.00%), Space: 42.8 MB (93.01%) - LeetHub

---
+++ 393-utf-8-validation/393-utf-8-validation.java
@@ -0,0 +1,36 @@
+class Solution {
+    public boolean validUtf8(int[] data) {
+        
+        int remainingBytes = 0;
+        for(int val : data) {
+            
+            if(remainingBytes == 0) {
+                if((val >> 7) == 0b0) {
+                    remainingBytes = 0;
+                }
+                else if((val >> 5) == 0b110) {
+                    remainingBytes = 1;
+                }
+                else if((val >> 4) == 0b1110) {
+                    remainingBytes = 2;
+                }
+                else if((val >> 3) == 0b11110) {
+                    remainingBytes = 3;
+                }
+                else {
+                    return false;
+                }
+            }
+            else {
+                if((val >> 6) == 0b10) {
+                    remainingBytes--;
+                }
+                else {
+                    return false;
+                }
+            }
+        }
+        
+        return remainingBytes == 0;
+    }
+}
